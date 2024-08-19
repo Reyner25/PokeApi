bfae48aa339962dd899141259c59a0d204640fef
unknown <reylatino250816@gmail.com>
2024-08-19 14:38:53 -0500

    continua la configuracion de GITPAGES

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://reyner25.github.io/PokeApi"
+  base: "https://reyner25.github.io/PokeApi/"
 })
